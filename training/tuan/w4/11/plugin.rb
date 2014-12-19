e6a2d878a00e04417bc76ca80fc90634456cf4db
tuanna0110 <tuanna@runsystem.net>
2014-12-19 13:50:50 +0900

    Week 4 Exercise

---
+++ training/tuan/w4/11/plugin.rb
@@ -0,0 +1,21 @@
+﻿#
+# Cookbook Name:: jenkins
+# Recipe:: plugin
+#
+
+# JenkinsのHomeディレクトリを定義する
+node.default["jenkins][home_dir"] = "/var/lib/jenkins"
+
+# JenkinsのPluginをダウンロードする
+%w{git xunit checkstyle pmd cloverphp dry htmlpublisher git-client scm-api analysis-core}.each do |plugin_name|
+	execute "download latest #{plugin_name} plugin" do
+	  cwd "#{node["jenkins][home_dir"]}/plugins"
+	  command "sudo wget -q http://updates.jenkins-ci.org/latest/#{plugin_name}.hpi"
+	  action :run
+	end
+end
+
+# Jenkinsのサービスを再起動する
+service "jenkins" do
+	action [:restart]
+end
